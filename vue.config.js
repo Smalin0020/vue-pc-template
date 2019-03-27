461de6d6215a405b878b70858b3a6e4996e2213c
smalin <dinglin0020@gmail.com>
2019-03-27 21:03:38 +0800

    add some config

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,16 @@ function resolve (dir) {
 }
 
 module.exports = {
+  devServer: {
+    port: 8080 // 端口自己改
+  },
   chainWebpack: config => {
     // 这里是对环境的配置，不同环境对应不同的BASE_API，以便axios的请求地址不同
     config.plugin('define').tap(args => {
       const argv = process.argv
       const mode = argv[argv.indexOf('--project-mode') + 1]
       args[0]['process.env'].MODE = `"${mode}"`
-      args[0]['process.env'].BASE_API = '"http://47.94.138.75:8000"'
+      args[0]['process.env'].BASE_API = '"啦啦啦啦"'
       return args
     })
 
