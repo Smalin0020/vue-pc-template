a733d219c8027ffd66e45af85b9dd0c5af3e1f25
Smalin <dinglin0020@gmail.com>
2019-05-21 15:25:24 +0800

    添加单元测试配置

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
+describe('测试组件', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
