484a2bf1b8bc77a9d28a022dea38b605b57550de
Smalin <dinglin0020@gmail.com>
2019-08-12 10:43:02 +0800

    添加一个日期格式化

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,5 +1,23 @@
-exports.install = function (Vue, options) {
-  Vue.prototype.test = function (date) {
-    alert(date)
+
+export default {
+  install (Vue, options) {
+    // 格式化时间
+    // formatterDate('YY-MM-DD HH:mm', date)
+    Vue.prototype.formatterDate = function (formatter, date) {
+      date = (date ? new Date(date) : new Date())
+      const Y = date.getFullYear() + ''
+      const M = date.getMonth() + 1
+      const D = date.getDate()
+      const H = date.getHours()
+      const m = date.getMinutes()
+      const s = date.getSeconds()
+      return formatter.replace(/YYYY|yyyy/g, Y)
+        .replace(/YY|yy/g, Y.substr(2, 2))
+        .replace(/MM/g, (M < 10 ? '0' : '') + M)
+        .replace(/DD/g, (D < 10 ? '0' : '') + D)
+        .replace(/HH|hh/g, (H < 10 ? '0' : '') + H)
+        .replace(/mm/g, (m < 10 ? '0' : '') + m)
+        .replace(/ss/g, (s < 10 ? '0' : '') + s)
+    }
   }
 }
